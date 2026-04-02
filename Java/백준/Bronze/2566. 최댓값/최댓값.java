d11ba121ea2f1b46bb5fba09a51a9a9da91dd3ee
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2026-04-02 16:20:52 +0900

    [Bronze III] Title: 최댓값, Time: 200 ms, Memory: 18496 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int arr[][] = new int[9][9];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(arr[i][j] == max){
+                    System.out.println(max);
+                    System.out.println((i + 1) + " " + (j + 1));
+                }
+            }
+        }
+    }
+}
