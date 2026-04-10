b9f5bcf4ca167b9f03a2bb6254ada232973ace9c
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2026-04-10 19:49:48 +0900

    [Bronze IV] Title: FYI, Time: 152 ms, Memory: 17656 KB -BaekjoonHub

---
+++ Java/백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String phoneNumber = sc.nextLine();
+        String cleanNumber = phoneNumber.replace("-", "");
+
+        if (cleanNumber.startsWith("555")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
