a9a6063afc3553610c09c4e33ffd209a3f1bf70a
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2026-04-11 12:55:05 +0900

    [Bronze V] Title: Above, Time: 184 ms, Memory: 17904 KB -BaekjoonHub

--- Java/백준/Bronze/34362. Above/Above.java
+++ Java/백준/Bronze/34362. Above/Above.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // 고도 입력 (실수형)
+
+        // 입력값이 있는지 확인 후 고도 N을 읽음
         if (sc.hasNextDouble()) {
             double n = sc.nextDouble();
-            
-            // 30cm(0.3m)만큼 고도가 낮아짐
+
+            // 현재 고도에서 해수면 상승치 0.3m를 뺌
             double result = n - 0.3;
-            
-            // 결과 출력
+
+            // 결과 출력 (소수점 아래 자릿수는 넉넉하게 출력해도 됨)
             System.out.println(result);
         }
-        
+
         sc.close();
     }
 }
